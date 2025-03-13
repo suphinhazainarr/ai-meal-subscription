00186e2b9ce28036235cdb8282826a304c73beb9
suphinhazainarr <suphinhassainar2004@gmail.com>
2025-03-13 17:05:45 +0530

    first

--- frontend/ai-meal/src/app/app.component.ts
+++ frontend/ai-meal/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignupComponent } from './components/auth//signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
