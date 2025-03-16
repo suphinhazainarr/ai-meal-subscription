e0caf1474894810edc783fc52c6891e65d860e23
suphinhazainarr <suphinhassainar2004@gmail.com>
2025-03-17 00:31:02 +0530

    nav finished

--- frontend/ai-meal/src/app/components/home/home.component.ts
+++ frontend/ai-meal/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   isNavbarExpanded = false;
+
+  ngOnInit() {
+    console.log('HomeComponent initialized'); // Debugging
+  }
 
   toggleNavbar() {
     this.isNavbarExpanded = !this.isNavbarExpanded;
+    console.log('Navbar toggled:', this.isNavbarExpanded); // Debugging
   }
 }
